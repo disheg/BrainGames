f3e1855a0ed9d16d25cdb1c04fe093361ca11bb1
Disheg <disheg@gmail.com>
2020-01-28 11:23:23 +0000

    l3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games');
+import userName from '..';
+const name = userName();
+console.log('Welcome to the Brain Games\n');
+console.log(`May I have your name? ${name}`);
+console.log(`Hello, ${name}`);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const userName = () => {
+	const name = readlineSync.question('Your answer: ');
+	return name;
+};
+
+export default userName;
