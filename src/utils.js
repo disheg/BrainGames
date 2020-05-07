c5cf208478799c16f743c22d388a658b44286aa9
disheg <57448151+disheg@users.noreply.github.com>
2020-05-07 22:07:46 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -4,10 +4,5 @@ const randomInteger = (min, max) => {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
-const greeting = () => {
-  console.log('Welcome to the Brain-games!\n');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-};
 
-export { randomInteger, greeting };
+export { randomInteger };
