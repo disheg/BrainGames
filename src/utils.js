2f18c82bb359ca3e8236083b8ebfdd093b375ce0
disheg <57448151+disheg@users.noreply.github.com>
2020-05-07 22:09:53 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,6 @@
-import readlineSync from 'readline-sync';
-
 const randomInteger = (min, max) => {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
 
-export { randomInteger };
+export randomInteger;
